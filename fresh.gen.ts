d0f31d22c1145074136b9d6b15e1b420723051b7
Jannik Keye <jannik.keye@gmail.com>
2022-11-04 10:14:04 +0100

    chore: rm old file

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -6,10 +6,9 @@ import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/api/joke.ts";
 import * as $1 from "./routes/index.tsx";
 import * as $2 from "./routes/properties/[id].tsx";
-import * as $$0 from "./islands/Counter.tsx";
-import * as $$1 from "./islands/PropertiesGrid.tsx";
-import * as $$2 from "./islands/PropertyDetail.tsx";
-import * as $$3 from "./islands/SimilarProperties.tsx";
+import * as $$0 from "./islands/PropertiesGrid.tsx";
+import * as $$1 from "./islands/PropertyDetail.tsx";
+import * as $$2 from "./islands/SimilarProperties.tsx";
 
 const manifest = {
   routes: {
@@ -18,10 +17,9 @@ const manifest = {
     "./routes/properties/[id].tsx": $2,
   },
   islands: {
-    "./islands/Counter.tsx": $$0,
-    "./islands/PropertiesGrid.tsx": $$1,
-    "./islands/PropertyDetail.tsx": $$2,
-    "./islands/SimilarProperties.tsx": $$3,
+    "./islands/PropertiesGrid.tsx": $$0,
+    "./islands/PropertyDetail.tsx": $$1,
+    "./islands/SimilarProperties.tsx": $$2,
   },
   baseUrl: import.meta.url,
   config,
